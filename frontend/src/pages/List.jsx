aed8e60715c53fb66f948f408232dfd681c32b30
Pesia Mittelman <pessie.mittelman@gmail.com>
2025-08-14 21:27:17 -0400

    adding api base ihchav4fx1

--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -13,7 +13,7 @@ function List() {
   const [newItem, setNewItem] = useState('');
   const [loading, setLoading] = useState(true);
 
-  const BASE_API_URL = import.meta.env.VITE_API_BASE_URL || 'https://h2fqo38sa8.execute-api.us-east-1.amazonaws.com';
+  const BASE_API_URL = import.meta.env.VITE_API_BASE_URL || 'https://ihchav4fx1.execute-api.us-east-1.amazonaws.com';
 
   const fetchList = async () => {
     try {
